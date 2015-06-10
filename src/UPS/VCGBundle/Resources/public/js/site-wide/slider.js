e384df4b97798d5d06e1e3820a5cee9d1448aad4
Justin Maurer <justin@Justins-MacBook-Pro.local>
2015-06-10 14:54:33 -0500

    slider changes

--- src/UPS/VCGBundle/Resources/public/js/site-wide/slider.js
+++ src/UPS/VCGBundle/Resources/public/js/site-wide/slider.js
@@ -13,7 +13,37 @@ $( document ).ready(function() {
     ItemWidth = $('.carousel-item img').width();
 
     NegMargin = -(ItemWidth - WindowWidth)/2;
-   $('.carousel-item img').css('margin-left', NegMargin);
+    $('.carousel-item img').css('margin-left', NegMargin);
+
+    //var tid = setTimeout(rotateSlider, 2000);
+    //function rotateSlider() {
+    //    // do some stuff...
+    //    $('')
+    //    tid = setTimeout(rotateSlider, 2000); // repeat myself
+    //}
+    //function abortTimer() { // to be called when you want to stop the timer
+    //    clearTimeout(tid);
+    //}
+    $('input[type="radio"]').each(function(){
+
+    })
+    var elements = $('input[type="radio"]','.carousel-inner');
+    var index = 0;
+
+    var doNext = null;
+    doNext = function() {
+        var element = elements.eq(index);
+        // do work
+        element.prop('checked',true);
+        if (index < elements.length) {
+            index++;
+            setTimeout(doNext, 6000);
+        } else {
+            index = 0;
+            setTimeout(doNext, 6000);
+        }
+    }
+    doNext();
 });
 
 /*
