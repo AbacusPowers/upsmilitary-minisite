2bd0fea7c2b31f790ebedec10778464378401e7d
csauls <ibisbasenji@gmail.com>
2015-06-11 10:51:56 -0500

    job map

--- src/UPS/VCGBundle/Resources/public/js/job-map/job-map.js
+++ src/UPS/VCGBundle/Resources/public/js/job-map/job-map.js
@@ -5,9 +5,9 @@
      */
     var CONST = {
         ATTR_PEG: [
-            { fill: '#d80000' }, // Large
-            { fill: '#0000d8' }, // Medium
-            { fill: '#00d800' }  // Small
+            { fill: '#d83030' }, // Large
+            { fill: '#3030d8' }, // Medium
+            { fill: '#30d848' }  // Small
         ],
 
         BASE_URL:           '/js/job-map/',
@@ -286,6 +286,7 @@
                 sm.llh = smc.top - smc.bottom;
                 sm.llw = smc.left - smc.right;
             }
+            console.log("submaps = ", this.submaps);
             
             var statesGroup = this.statesGroup = snap.select('g.svg-states-group');
             statesGroup.transform('0,0,0,0,0,0');
