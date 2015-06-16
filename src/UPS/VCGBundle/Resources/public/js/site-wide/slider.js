f5e5305528008ce6d2c8788ef33e992b5ab5764e
Scott Arenz <scott.arenz@iostudio.com>
2015-06-15 19:39:09 -0500

    slider styles

--- src/UPS/VCGBundle/Resources/public/js/site-wide/slider.js
+++ src/UPS/VCGBundle/Resources/public/js/site-wide/slider.js
@@ -1,46 +1,46 @@
 
-$( document ).ready(function() {
-    /**
-     * CENTER IMAGES ON PAGE LOAD
-     */
-    WindowWidth = $(window).width();
-    ItemWidth = $('.carousel-item img').width();
+// $( document ).ready(function() {
+//     /**
+//      * CENTER IMAGES ON PAGE LOAD
+//      */
+//     WindowWidth = $(window).width();
+//     ItemWidth = $('.carousel-item img').width();
 
-    NegMargin = -(ItemWidth - WindowWidth)/2;
-    $('.carousel-item img').css('margin-left', NegMargin);
+//     NegMargin = -(ItemWidth - WindowWidth)/2;
+//     $('.carousel-item img').css('margin-left', NegMargin);
 
-    /**
-     * AUTO ROTATE
-     */
-    var elements = $('.carousel-inner input[type="radio"]');
-    var index = 0;
+//     /**
+//      * AUTO ROTATE
+//      */
+//     var elements = $('.carousel-inner input[type="radio"]');
+//     var index = 0;
 
-    var doNext = null;
-    doNext = function() {
-        //if(item)index = item;
+//     var doNext = null;
+//     doNext = function() {
+//         //if(item)index = item;
 
-        var element = elements.eq(index);
-        var x = setTimeout(doNext, 6000);
-        // do work
+//         var element = elements.eq(index);
+//         var x = setTimeout(doNext, 6000);
+//         // do work
 
-        $('.carousel-inner input[type="radio"]').change(function(){
-            newIndex = $(this).index('.carousel-inner input[type="radio"]');
-            index = newIndex;
-        });
+//         $('.carousel-inner input[type="radio"]').change(function(){
+//             newIndex = $(this).index('.carousel-inner input[type="radio"]');
+//             index = newIndex;
+//         });
 
-        element.prop('checked',true);
-        if (index < elements.length) {
-            index++;
-            x;
-        } else {
-            index = 0;
-            x;
-        }
-    }
-    doNext();
+//         element.prop('checked',true);
+//         if (index < elements.length) {
+//             index++;
+//             x;
+//         } else {
+//             index = 0;
+//             x;
+//         }
+//     }
+//     doNext();
 
 
-});
+// });
 
 /**
  * CENTER IMAGES ON WINDOW RESIZE
