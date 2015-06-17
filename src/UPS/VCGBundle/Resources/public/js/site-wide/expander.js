d957d91eae0dc9422f76f9f116b5bec4265f1636
Justin Maurer <justin@Justins-MacBook-Pro.local>
2015-06-17 13:38:13 -0500

    the great expander fix

--- src/UPS/VCGBundle/Resources/public/js/site-wide/expander.js
+++ src/UPS/VCGBundle/Resources/public/js/site-wide/expander.js
@@ -1,28 +1,39 @@
 $('document').ready(function(){
+
     $('.expand-button','.veteranGateway').click(function(){
-        $height = $(this).siblings('.hidden-part').children('.expander__child').height();
-        $(this).parent('.expander__wrapper').addClass('open-expander');
-        $(this).siblings('.hidden-part').velocity("transition.slideDownBigIn");
-        $(this).hide();
-        $(this).siblings('.hide-button').show();
-    });
-    $('.hide-button','.veteranGateway').click(function(){
-        $(this).parent('.expander__wrapper').removeClass('open-expander');
-        $(this).siblings('.hidden-part').velocity("transition.slideUpBigOut");
-        $(this).hide();
-        $(this).siblings('.expand-button').show();
+        $(this).parent('.expander__wrapper').toggleClass('open-expander');
+        $(this).siblings('.hidden-part').slideToggle();
+        $('i.fa',this).toggleClass('fa-plus').toggleClass('fa-minus');
     });
     $('#modal').on('click','.expand-button',function(){
-        $height = $(this).siblings('.hidden-part').children('.expander__child').height();
-        $(this).parent('.expander__wrapper').addClass('open-expander');
-        $(this).siblings('.hidden-part').velocity({height: $height}, 400).velocity({opacity: 1});
-        $(this).hide();
-        $(this).siblings('.hide-button').show();
+        $(this).parent('.expander__wrapper').toggleClass('open-expander');
+        $(this).siblings('.hidden-part').slideToggle();
+        $('i.fa',this).toggleClass('fa-plus').toggleClass('fa-minus');
     });
-    $('#modal').on('click','.hide-button',function(){
-        $(this).parent('.expander__wrapper').removeClass('open-expander');
-        $(this).siblings('.hidden-part').velocity({opacity: 0}).velocity({height: 0}, 400);
-        $(this).hide();
-        $(this).siblings('.expand-button').show();
-    });
+    //$('.hide-button','.veteranGateway').click(function(){
+    //    $(this).parent('.expander__wrapper').removeClass('open-expander');
+    //    $(this).siblings('.hidden-part').slideUp();
+    //    //$(this).siblings('.hidden-part').velocity("transition.slideUpBigOut");
+    //    //$(this).hide();
+    //    //$(this).siblings('.expand-button').show();
+    //    $(this).removeClass('hide-button').addClass('expand-button');
+    //    $('i.fa',this).removeClass('fa-minus').addClass('fa-plus');
+    //});
+
+    //$('#modal').on('click','.expand-button',function(){
+    //    $height = $(this).siblings('.hidden-part').children('.expander__child').height();
+    //    $(this).parent('.expander__wrapper').addClass('open-expander');
+    //    $(this).siblings('.hidden-part').slideDown();
+    //    $(this).hide();
+    //    $(this).siblings('.hide-button').show();
+    //});
+    //$('#modal').on('click','.hide-button',function(){
+    //    $(this).parent('.expander__wrapper').removeClass('open-expander');
+    //    $(this).siblings('.hidden-part').slideUp();
+    //    $(this).hide();
+    //    $(this).siblings('.expand-button').show();
+    //});
+    //$('.expander__parent').addClass('bellows__header');
+    //$('.expander__wrapper').addClass('bellows__item');
+    //$('.component--expander').bellows();
 });
