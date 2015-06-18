3d7a245139875047a4b0bf657def41ff654a422b
Justin Maurer <justin@Justins-iMac.local>
2015-06-18 17:58:26 -0500

    dd214 coolness complete

--- src/UPS/VCGBundle/Resources/public/js/site-wide/dd214.js
+++ src/UPS/VCGBundle/Resources/public/js/site-wide/dd214.js
@@ -32,3 +32,37 @@ $( function(){
         svg.animate({'opacity': 0}, 300);
     });
 });
+
+$( function(){
+    $('.svg-target').hover(
+        function(){ //in
+            var id = $(this).attr('data-id');
+            $('.button[data-id="'+ id +'"]').addClass('hover');
+            var svg = Snap.select('#block'+id);
+            svg.animate({'opacity': 1}, 300);
+        },
+        function(){ //out
+            var id = $(this).attr('data-id');
+            $('.button[data-id="'+ id +'"]').removeClass('hover');
+            var svg = Snap.select('#block'+id);
+            if(!$(this).parent('.expander__wrapper').hasClass('open-expander')){
+                svg.animate({'opacity': 0}, 300);
+            }
+        });
+});
+$( function(){
+    $('.svg-target','.page--ddform214').click(function(){
+        var id = $(this).attr('data-id');
+        target = $('.button[data-id="'+id+'"]');
+        target.parent('.expander__wrapper').toggleClass('open-expander');
+        target.siblings('.hidden-part').slideToggle();
+        target.children('i.fa').toggleClass('fa-plus').toggleClass('fa-minus');
+
+        var svg = Snap.select('#block'+id);
+        if(target.hasClass('expand-button')) {
+            svg.animate({'opacity': 1}, 300);
+        } else if (target.hasClass('hide-button')) {
+            svg.animate({'opacity': 0}, 300);
+        }
+    });
+});
