15fe2e9e237f1d60faa8afad022a93f4acb1d12d
Justin Maurer <justin@Justins-iMac.local>
2015-06-19 16:01:23 -0500

    sticky dd214

--- src/UPS/VCGBundle/Resources/public/js/site-wide/expander.js
+++ src/UPS/VCGBundle/Resources/public/js/site-wide/expander.js
@@ -2,12 +2,16 @@ $('document').ready(function(){
 
     $('.expand-button','.veteranGateway').click(function(){
         $(this).parent('.expander__wrapper').toggleClass('open-expander');
-        $(this).siblings('.hidden-part').slideToggle();
+        $(this).siblings('.hidden-part').slideToggle(function(){
+            $(this).parent('.expander__wrapper').trigger('classChange');
+        });
         $(this).children('i.fa').toggleClass('fa-plus').toggleClass('fa-minus');
     });
     $('#modal').on('click','.expand-button',function(){
-        $(this).parent('.expander__wrapper').toggleClass('open-expander');
-        $(this).siblings('.hidden-part').slideToggle();
+        $(this).parent('.expander__wrapper').toggleClass('open-expander').trigger('classChange');
+        $(this).siblings('.hidden-part').slideToggle(function(){
+            $(this).parent('.expander__wrapper').trigger('classChange');
+        });
         $(this).children('i.fa').toggleClass('fa-plus').toggleClass('fa-minus');
     });
     //$('.hide-button','.veteranGateway').click(function(){
