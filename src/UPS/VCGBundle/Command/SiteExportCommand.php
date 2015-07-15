99f309eca19e123ff3d3a3d3b22b2755e01d9831
Justin Maurer <justin@Justins-MacBook-Pro.local>
2015-07-15 14:59:44 -0500

    changes

--- src/UPS/VCGBundle/Command/SiteExportCommand.php
+++ src/UPS/VCGBundle/Command/SiteExportCommand.php
@@ -32,7 +32,68 @@ class SiteExportCommand extends ContainerAwareCommand
      */
     private $urls = array(
         '/transition-guide/dd-form-214-guide.html',
-        '/transition-guide/not-a-real-page'
+        '/index.html',
+        '/article/brand-intro-home.html',
+        '/video/feeder-driver.html',
+        '/video/frontline-supervisor.html',
+        '/video/hr.html',
+        '/video/cover-driver.html',
+        '/video/mechanic.html',
+        '/video/otr-driver.html',
+        '/video/package-driver.html',
+        '/video/sorter.html',
+        '/video/technician.html',
+        '/jobs/video/feeder-driver.html',
+        '/jobs/video/frontline-supervisor.html',
+        '/jobs/video/hr.html',
+        '/jobs/video/cover-driver.html',
+        '/jobs/video/mechanic.html',
+        '/jobs/video/otr-driver.html',
+        '/jobs/video/package-driver.html',
+        '/jobs/video/sorter.html',
+        '/jobs/video/technician.html',
+        '/transition-guide/video/resume.html',
+        '/transition-guide/video/networking.html',
+        '/transition-guide/video/social-media.html',
+        '/transition-guide/video/interview.html',
+        '/culture-benefits/video/feeder-driver.html',
+        '/culture-benefits/video/frontline-supervisor.html',
+        '/culture-benefits/video/hr.html',
+        '/culture-benefits/video/cover-driver.html',
+        '/culture-benefits/video/mechanic.html',
+        '/culture-benefits/video/otr-driver.html',
+        '/culture-benefits/video/package-driver.html',
+        '/culture-benefits/video/sorter.html',
+        '/culture-benefits/video/technician.html',
+        '/transition-guide.html',
+        '/transition-guide/dd-form-214-guide.html',
+        '/transition-guide/article/hiring-resources.html',
+        '/transition-guide/article/brand-intro.html',
+        '/transition-guide/article/cover-letter-resume.html',
+        '/transition-guide/article/marketing-yourself.html',
+        '/transition-guide/article/networking-essentials.html',
+        '/transition-guide/article/recruiters.html',
+        '/transition-guide/article/career-fair.html',
+        '/transition-guide/article/interview.html',
+        '/events.html',
+        '/job-map.html',
+        '/jobs.html',
+        '/job-converter.html',
+        '/culture-benefits.html',
+        '/culture-benefits/article/faq.html',
+        '/culture-benefits/article/values.html',
+        '/culture-benefits/article/family-programs.html',
+        '/culture-benefits/article/benefits.html',
+        '/culture-benefits/article/student-programs.html',
+        '/culture-benefits/article/esgr.html',
+        '/culture-benefits/article/ra-intro.html',
+        '/culture-benefits/article/ra-application.html',
+        '/culture-benefits/article/ra-responsibilities.html',
+        '/culture-benefits/article/ra-gi-benefits.html',
+        '/culture-benefits/article/ra-post-911-payments.html',
+        '/culture-benefits/article/ra-mgib-payments.html',
+        '/culture-benefits/article/ra-glossary.html',
+        '/culture-benefits/article/ra-faq.html'
     );
 
     /**
@@ -63,7 +124,7 @@ EOT
         $client = $kernel->getContainer()->get( 'test.client' );
         $dryRun = $input->getOption('dry-run');
         $this->writeSite($client, $dryRun, $output);
-        $output->writeln('Everything is fine');
+        $output->writeln('Site export complete');
     }
 
 
@@ -116,19 +177,18 @@ EOT
                     $output->writeln('Folder path ' . $folderPath . ' will be created.' );
                 }
                 try {
-                    $response = $client->request( 'GET', $url );
+                    $client->request( 'GET', $url );
                     $statusCode = $client->getResponse()->getStatusCode();
                     if ($statusCode != 200) {
                         throw new \Exception("Page Does Not Exist", 3489);
                     }
-                    var_dump($statusCode);
+//                    var_dump($statusCode);
 //                    $output->writeln($response);
                     $output->writeln('<fg=green>'.$slug . ' will be written to ' . $folderPath.$slug.'</fg=green>' );
                 } catch (\Exception $ex) {
                     $output->writeln('<fg=red>Nothing found at ' . $url .'</fg=red>' );
                 }
             } else {
-                $this->dumpAssets($output);
                 try {
                     //create appropriate folder in ROOT/site-export/
                     $fs->mkdir($folderPath);
@@ -154,7 +214,8 @@ EOT
 
         if($dryRun != true){
             $this->dumpAssets($output);
-            $fs->copy('web/favicon.ico','site-export/favicon.ico')
+            $fs->copy('web/favicon.ico','site-export/favicon.ico');
+            $fs->copy('web/robots.txt','site-export/robots.txt');
             $fs->mirror('web/css', 'site-export/css');
             $fs->mirror('web/js', 'site-export/js');
             $fs->mirror('web/bundles', 'site-export/bundles');
