3f928e6d139dd06ed9277784bdfaf882310463a4
Justin Maurer <justin@Justins-MacBook-Pro.local>
2015-07-29 15:13:06 -0500

    translator functionality done

--- src/UPS/VCGBundle/Resources/public/js/military-skills-translator/translator.js
+++ src/UPS/VCGBundle/Resources/public/js/military-skills-translator/translator.js
@@ -8,17 +8,29 @@ function aContainsB (a, b) {
 }
 
 
-var bucketDescriptions = [];
-bucketDescriptions[0] = 'Bucket 0 description';
-bucketDescriptions[1] = 'Bucket 1 description';
-bucketDescriptions[2] = 'Bucket 2 description';
-bucketDescriptions[3] = 'Bucket 3 description';
-bucketDescriptions[4] = 'Bucket 4 description';
-bucketDescriptions[5] = 'Bucket 5 description';
-bucketDescriptions[6] = 'Bucket 7 description';
-bucketDescriptions[7] = 'Bucket 8 description';
-bucketDescriptions[8] = 'Bucket 8 description';
-bucketDescriptions[9] = 'Bucket 9 description';
+//var bucketDescriptions = [];
+//bucketDescriptions[0] = 'Bucket 0 description';
+//bucketDescriptions[1] = 'Bucket 1 description';
+//bucketDescriptions[2] = 'Bucket 2 description';
+//bucketDescriptions[3] = 'Bucket 3 description';
+//bucketDescriptions[4] = 'Bucket 4 description';
+//bucketDescriptions[5] = 'Bucket 5 description';
+//bucketDescriptions[6] = 'Bucket 7 description';
+//bucketDescriptions[7] = 'Bucket 8 description';
+//bucketDescriptions[8] = 'Bucket 8 description';
+//bucketDescriptions[9] = 'Bucket 9 description';
+
+var buckets = [];
+buckets[0] = {name: 'No direct match', description: 'No direct match available'};
+buckets[1] = {name: 'Delivery Drivers', description: 'Delivery driver bucket description'};
+buckets[2] = {name: 'Freight Drivers', description: 'Freight driver bucket description'};
+buckets[3] = {name: 'Logistics', description: 'Logistics bucket description'};
+buckets[4] = {name: 'Operations', description: 'Operations bucket description'};
+buckets[5] = {name: 'Mechanics and Technicians', description: 'Mechanics and technicians bucket description'};
+buckets[6] = {name: 'Administrative Workers', description: 'Admin bucket description'};
+buckets[7] = {name: 'Professional Workers', description: 'Professional bucket description'};
+buckets[8] = {name: 'Information Systems', description: 'Information systems bucket description'};
+buckets[9] = {name: 'Air Operations', description: 'Air operations bucket description'};
 
 
 function search(nameKey, myArray){
@@ -27,6 +39,7 @@ function search(nameKey, myArray){
             return myArray[i];
         }
     }
+    return {};
 }
 
 function buildHints(branchKey, myArray){
@@ -51,6 +64,25 @@ function checkBranch(val){
     }
     console.log('value: ' + val + ' matches item ' + hints.indexOf(val));
 }
+
+function shuffle(array) {
+    var m = array.length, t, i;
+
+    // While there remain elements to shuffle…
+    while (m) {
+
+        // Pick a remaining element…
+        i = Math.floor(Math.random() * m--);
+
+        // And swap it with the current element.
+        t = array[m];
+        array[m] = array[i];
+        array[i] = t;
+    }
+
+    return array;
+}
+
 $( document ).ready(function() {
     $.get("/bundles/vcg/data/new-jobs-data.csv", function(data){
         //        console.log(data);
@@ -79,6 +111,80 @@ $( document ).ready(function() {
     $('#branch-of-service').on('autocompletechange',function(){
         var val = $(this).val();
         checkBranch(val);
+    });
+    $('#military-skills-translator').submit(function(e){
+        e.preventDefault();
+        //hide old search results
+        $('#side--a').addClass('full-width');
+        $('#form-error').addClass('hidden');
+        $('#form-error').text('');
+        $('#military-job').addClass('hidden');
+        $('#job-types').addClass('hidden');
+        //$('.component--expander').addClass('hidden');
+
+        $('.wrap--match-bucket').addClass('hidden');
+        $('.wrap--no-match-bucket').addClass('hidden');
+        $('.wrap--all-buckets .component--expander').text('');
+        $('.wrap--all-buckets').addClass('hidden');
+
+
+        //searh value
+        var searchString = $('#job-code').val();
+
+        //search
+        var result = search(searchString, allJobs);
+        if (!$.isEmptyObject(result)) {
+            //we have a result!
+            //set the military job title
+            $('#military-job-title').text(result.Title);
+
+            //fill in bucket info
+            var b = result.Bucket;
+            if (b > 0) {
+                var bucketTitle = buckets[b].name;
+                var bucketDescription = buckets[b].description;
+                $('.wrap--match-bucket .expander__parent').text(bucketTitle);
+                $('.wrap--match-bucket .expander__child p').text(bucketDescription);
+                //reveal direct ma
+                $('.wrap--match-bucket').removeClass('hidden');
+                console.log('direct match with bucket  ' + bucketTitle);
+            } else {
+                $('.wrap--no-match-bucket').removeClass('hidden');
+                console.log('no direct match');
+            }
+
+            //generate other buckets
+            var otherBuckets = [];
+            for( i = 1; i < buckets.length; i++) {
+
+                //exclude the matched bucket
+                if (i != b) {
+                    otherBuckets.push(buckets[i]);
+                }
+            }
+            //randomize buckets
+            var randBuckets = shuffle(otherBuckets);
+            console.log(randBuckets);
+            //loop through randomized bucket list
+            for( i = 0; i < randBuckets.length; i++) {
+                var randBucketTitle = randBuckets[i].name;
+                var randBucketDescription = randBuckets[i].description;
+                $('.wrap--all-buckets .component--expander').append('<section class="expander__wrapper"><a data-id="' + i + '" class="button expand-button"><h2 class="expander__parent">' + randBucketTitle + '</h2><i class="fa fa-plus fa-2x"></i></a><div data-id="' + i + '" class="hidden-part"><div class="expander__child"><p class="">' + randBucketDescription + '</p><form class="job-search--military-skills-translator"><h4>Search to see openings for this job near you:</h4><input type="submit" value="Search" /></form></div></div></section>');
+            }
+            //reveal randomized bucket list
+            $('.wrap--all-buckets').removeClass('hidden');
+
+
+            //reveal all relevant divs
+
+            //$('.wrap--match-bucket .expander__wrapper').removeClass('hidden');
+            //$('.wrap--match-bucket .component--expander').removeClass('hidden');
+
+
+        } else {
+            //no bucket match
+        }
+
     });
     //
 
