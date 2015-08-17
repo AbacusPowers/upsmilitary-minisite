60fc48f6fd7d2a9e50cf535fb178b13b94ec90df
Micah Breedlove <druid628@gmail.com>
2015-08-17 18:07:52 -0500

    removed gulp-imagemin

--- gulp/svg/optimize.js
+++ gulp/svg/optimize.js
@@ -2,7 +2,7 @@ module.exports = function( path ){
     'use strict';
     var gulp = require('gulp'),
         svgspritesheet = require('./gulp-svg-spritesheet'),
-        imagemin = require('gulp-imagemin'),
+//        imagemin = require('gulp-imagemin'),
         inquirer = require('inquirer'),
         cheerio = require('gulp-cheerio');
 
@@ -61,55 +61,55 @@ module.exports = function( path ){
         .on('finish', function(){ done(); });
     });
 
-    gulp.task('optimize:sprite', false, ['spriteify:svg' ], function( done ) {
-        gulp.src( 'src/chamber/DashboardBundle/Resources/public/svg/sprite.svg' )
-            .pipe(cheerio({
-                run: prepareSprite,
-                parserOptions: {
-                  xmlMode: true
-                }
-            }))
-            .pipe(imagemin({
-                plugins: [
-                // SVGOptimizer Plugin list
-                // https://github.com/svg/svgo/tree/master/plugins
-                {cleanupAttrs: true},
-                {cleanupEnableBackground: true},
-                {cleanupIDs: false},
-                {cleanupListOfValues: false},
-                {cleanupNumericValues: true},
-                {collapseGroups: true},
-                {convertColors: true},
-                {convertPathData: false},
-                {convertShapeToPath: true},
-                {convertStyleToAttrs: true},
-                {convertTransform: false},
-                {mergePaths: true},
-                {moveElemsAttrsToGroup: true},
-                {moveGroupAttrsToElems: false},
-                {removeComments: true},
-                {removeDesc: false},
-                {removeDoctype: true},
-                {removeEditorsNSData: true},
-                {removeEmptyAttrs: true},
-                {removeEmptyContainers: true},
-                {removeEmptyText: true},
-                {removeHiddenElems: false},
-                {removeMetadata: true},
-                {removeNonInheritableGroupAttrs: true},
-                {removeRasterImages: true},
-                {removeTitle: false},
-                {removeUnknownsAndDefaults: true},
-                {removeUnusedNS: true},
-                {removeUselessStrokeAndFill: true},
-                {removeViewBox: false},
-                {removeXMLProcInst: true},
-                {sortAttr: true},
-                {transformsWithOnePath: false}
-                ]
-            }))
-            .pipe(gulp.dest( 'src/chamber/DashboardBundle/Resources/public/svg/' ))
-            .on('finish', function(){ done(); });
-        });
-
+//      gulp.task('optimize:sprite', false, ['spriteify:svg' ], function( done ) {
+//          gulp.src( 'src/chamber/DashboardBundle/Resources/public/svg/sprite.svg' )
+//              .pipe(cheerio({
+//                  run: prepareSprite,
+//                  parserOptions: {
+//                    xmlMode: true
+//                  }
+//              }))
+//              .pipe(imagemin({
+//                  plugins: [
+//                  // SVGOptimizer Plugin list
+//                  // https://github.com/svg/svgo/tree/master/plugins
+//                  {cleanupAttrs: true},
+//                  {cleanupEnableBackground: true},
+//                  {cleanupIDs: false},
+//                  {cleanupListOfValues: false},
+//                  {cleanupNumericValues: true},
+//                  {collapseGroups: true},
+//                  {convertColors: true},
+//                  {convertPathData: false},
+//                  {convertShapeToPath: true},
+//                  {convertStyleToAttrs: true},
+//                  {convertTransform: false},
+//                  {mergePaths: true},
+//                  {moveElemsAttrsToGroup: true},
+//                  {moveGroupAttrsToElems: false},
+//                  {removeComments: true},
+//                  {removeDesc: false},
+//                  {removeDoctype: true},
+//                  {removeEditorsNSData: true},
+//                  {removeEmptyAttrs: true},
+//                  {removeEmptyContainers: true},
+//                  {removeEmptyText: true},
+//                  {removeHiddenElems: false},
+//                  {removeMetadata: true},
+//                  {removeNonInheritableGroupAttrs: true},
+//                  {removeRasterImages: true},
+//                  {removeTitle: false},
+//                  {removeUnknownsAndDefaults: true},
+//                  {removeUnusedNS: true},
+//                  {removeUselessStrokeAndFill: true},
+//                  {removeViewBox: false},
+//                  {removeXMLProcInst: true},
+//                  {sortAttr: true},
+//                  {transformsWithOnePath: false}
+//                  ]
+//              }))
+//              .pipe(gulp.dest( 'src/chamber/DashboardBundle/Resources/public/svg/' ))
+//              .on('finish', function(){ done(); });
+//          });
+//  
 };
